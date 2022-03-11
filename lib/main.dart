c9f424cc05c30c939567467029aba5d8c40ed30c
Saldi Sucipto <saldisucipto@gmail.com>
2022-03-11 12:51:41 +0700

    Text Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Kita menggunkan null safety dan menggunkan const
-    return const MaterialApp(
+    return MaterialApp(
       // Menghilangkan banner debug
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         // ini adalah widget tampilan yang akan digunakan
+        appBar: AppBar(
+          title: const Text("Aplikasi Pertama"),
+        ),
         body: Center(
           // ini widget untuk menempatkan semuanya ditengah
           // body adalah parameter dari scaffold widget untuk memberikan konten pada body aplikasi kita
-          child: Text("This is A Hello World Application"),
+          child: Container(
+            color: Colors.blue,
+            width: 150,
+            height: 50,
+            child: const Text(
+              "This is A Hello World Application lorem ipsum dolor sit ammet",
+              // Mengatur batas maksimal text
+              maxLines: 2,
+              // ,mepunyai properties overflow juga
+              overflow: TextOverflow.ellipsis,
+              softWrap: false,
+              // mengatur align text
+              textAlign: TextAlign.center,
+              // Styling Text
+              style: TextStyle(
+                // merubah warna text
+                color: Colors.white,
+                // Merubah ketebalan text
+                fontWeight: FontWeight.bold,
+                // Merubah ukuran Text
+                fontSize: 20,
+              ),
+            ),
+          ),
         ),
       ),
     );
