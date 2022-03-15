e9c4f05707f333a1feb83c98e3c2ae6dd65f9b90
Saldi Sucipto <saldisucipto@gmail.com>
2022-03-15 19:31:47 +0700

    StateFull Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,133 +6,113 @@ void main() {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void aksiTombol() {
+    number++;
+    // fungsi setState berfungsi untul melakukan dan mendeteksi perubahan pada layar
+    setState(() {
+      number;
+    });
+    print(number);
+  }
+
+  int number = 0;
+
+  @override
   Widget build(BuildContext context) {
-    // Kita menggunkan null safety dan menggunkan const
-    var ukuran = MediaQuery.of(context).size;
-    return MaterialApp(
-      // Menghilangkan banner debug
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        // ini adalah widget tampilan yang akan digunakan
-        appBar: AppBar(
-          title: const Text("Aplikasi Pertamaas"),
-        ),
-        body: Center(
-          child: Column(
-            // Column akan membuat sebuah penempatan list secara menurun atau horizontal
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            // Untuk Mengatur Content pada Kolom dimulai dengan start
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blueAccent,
-                ),
-                width: 100,
-                height: 50,
-                child: const Center(
-                  child: Text(
-                    "Box 1",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("State Full Widgets Demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                number.toString(),
+                style: const TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 50),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blueAccent,
-                ),
-                width: 100,
-                height: 50,
-                child: const Center(
-                  child: Text(
-                    "Box 2",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: aksiTombol,
+                  child: Container(
+                    width: 100,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blueAccent,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "+",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.blueAccent,
-                ),
-                width: 100,
-                height: 50,
-                child: const Center(
-                  child: Text(
-                    "Box 3",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(color: Colors.white),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      if (number < 0) {
+                        number *= -1;
+                      }
+                      number = 1;
+                      number--;
+                    });
+                  },
+                  child: Container(
+                    width: 100,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blueAccent,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "-",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    // color: Colors.blueAccent,
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      number = 0;
+                    });
+                  },
+                  child: Container(
+                    width: 100,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.redAccent),
+                    child: const Center(
+                      child: Text(
+                        "0",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
                   ),
-                  height: 50,
-                  width: ukuran.width,
-                  child: Row(
-                    // Mangatur Space Pada Element
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blueAccent,
-                        ),
-                        width: 100,
-                        height: 50,
-                        child: const Center(
-                          child: Text(
-                            "Box 4",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blueAccent,
-                        ),
-                        width: 100,
-                        height: 50,
-                        child: const Center(
-                          child: Text(
-                            "Box 5",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blueAccent,
-                        ),
-                        width: 100,
-                        height: 50,
-                        child: const Center(
-                          child: Text(
-                            "Box 6",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
