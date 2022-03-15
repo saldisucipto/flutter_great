bd0e94db3a654ed7a2c1edaf8914ca2beb198b81
Saldi Sucipto <saldisucipto@gmail.com>
2022-03-15 21:19:52 +0700

    List And ListView

--- lib/main.dart
+++ lib/main.dart
@@ -14,107 +14,42 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  void aksiTombol() {
-    number++;
-    // fungsi setState berfungsi untul melakukan dan mendeteksi perubahan pada layar
-    setState(() {
-      number;
-    });
-  }
-
-  int number = 0;
-
+  List<Widget> datas = [];
+  int counter = 1;
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("State Full Widgets Demo"),
+        title: Text("Belajar List View"),
       ),
-      body: Center(
-        child: Column(
+      body: ListView(children: [
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: Text(
-                number.toString(),
-                style: const TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 50),
-              ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  datas.add(Text("Data Ke " + counter.toString()));
+                  counter++;
+                });
+              },
+              child: const Text("Add Me+"),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                  onPressed: aksiTombol,
-                  child: Container(
-                    width: 100,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.blueAccent,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "+",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      if (number < 0) {
-                        number *= -1;
-                      }
-                      number = 1;
-                      number--;
-                    });
-                  },
-                  child: Container(
-                    width: 100,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.blueAccent,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "-",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-                TextButton(
-                  // Menggunkan Anonymous Widget
-                  onPressed: () {
-                    setState(() {
-                      number = 0;
-                    });
-                  },
-                  child: Container(
-                    width: 100,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.redAccent),
-                    child: const Center(
-                      child: Text(
-                        "0",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  datas.removeLast();
+                });
+              },
+              child: const Text("Delete Me-"),
             ),
           ],
         ),
-      ),
+        Column(
+          children: datas,
+        )
+      ]),
     );
   }
 }
