8adadfc18461a66c1961970c6f2fae48406e4793
Saldi Sucipto <saldisucipto@gmail.com>
2022-03-21 14:26:52 +0700

    TextField Decorations

--- lib/pages/widgets/textfiels_widgtes.dart
+++ lib/pages/widgets/textfiels_widgtes.dart
@@ -18,8 +18,26 @@ class _FormWidgetsState extends State<FormWidgets> {
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8),
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
             child: TextField(
+              // untuk memberikan style pada inputan
+              decoration: InputDecoration(
+                // membuat border radius
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                // Memberikan Icon Disambing Filed
+                icon: const Icon(Icons.usb_off),
+                // memberikan icon pada colom
+                prefixIcon: const Icon(Icons.usb),
+                // memberikan label pada textfield
+                labelText: "USB Dicabut",
+                // Memberikan Placeholder
+                hintText: "Usb Akan DIcabut",
+                // Mmebrikan warna pada saat di ipnut
+                fillColor: Colors.cyanAccent[50],
+                filled: true,
+              ),
               controller: controller,
               onChanged: (value) {
                 setState(() {});
