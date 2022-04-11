2f5c50cfc1b50aa5356ec02920499d58944ce68a
Saldi Sucipto <saldisucipto@gmail.com>
2022-04-12 05:28:58 +0700

    Just Create First Ui

---
+++ lib/ui/home_pages.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatefulWidget {
+  const HomePages({Key? key}) : super(key: key);
+
+  @override
+  State<HomePages> createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Our Home Pages"),
+      ),
+    );
+  }
+}
